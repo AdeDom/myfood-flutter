a99cbd2d8866d8b69e852d0267cf91e4455a8d05
pathiphon <pathiphon.ja@chococrm.com>
2022-04-28 22:08:35 +0700

    Build widget language th button.

--- lib/main.dart
+++ lib/main.dart
@@ -104,6 +104,7 @@ class MyApp extends StatelessWidget {
           const SizedBox(
             height: 24,
           ),
+          _buildLanguageThButton(),
         ],
       ),
     );
@@ -173,6 +174,74 @@ class MyApp extends StatelessWidget {
             color: Color.fromARGB(230, 255, 215, 0),
             fontWeight: FontWeight.bold,
           ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLanguageThButton() {
+    return Stack(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  width: 80,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    border: Border.all(
+                      width: 1,
+                      color: const Color.fromARGB(230, 255, 215, 0),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 40,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: const Color.fromARGB(230, 255, 215, 0),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: const [
+                SizedBox(height: 4,),
+                Text(
+                  "TH",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 14,
+            ),
+            Column(
+              children: const [
+                SizedBox(height: 4,),
+                Text(
+                  "EN",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ],
     );
