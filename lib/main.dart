e3b17193be52d13a8578648fd41f913f0b8f64b1
pathiphon <pathiphon.ja@chococrm.com>
2022-04-28 20:21:43 +0700

    Build widget login button.

--- lib/main.dart
+++ lib/main.dart
@@ -31,54 +31,52 @@ class MyApp extends StatelessWidget {
   Widget _buildTopSection() {
     return Expanded(
       flex: 7,
-      child: Container(
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(32),
-                  bottomRight: Radius.circular(32),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.shade500,
-                    spreadRadius: 5,
-                    blurRadius: 10,
-                  )
-                ],
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(32),
+                bottomRight: Radius.circular(32),
               ),
-              child: const ClipRRect(
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(32),
-                  bottomRight: Radius.circular(32),
-                ),
-                child: Image(
-                  image: AssetImage("assets/images/bg_welcome.png"),
-                  alignment: Alignment.center,
-                  height: double.infinity,
-                  width: double.infinity,
-                  fit: BoxFit.fill,
-                ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade500,
+                  spreadRadius: 5,
+                  blurRadius: 10,
+                )
+              ],
+            ),
+            child: const ClipRRect(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(32),
+                bottomRight: Radius.circular(32),
+              ),
+              child: Image(
+                image: AssetImage("assets/images/bg_welcome.png"),
+                alignment: Alignment.center,
+                height: double.infinity,
+                width: double.infinity,
+                fit: BoxFit.fill,
               ),
             ),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    "assets/images/logo_black.png",
-                    width: 200,
-                  ),
-                  Image.asset(
-                    "assets/images/logo_food_delivery.png",
-                    width: 110,
-                  ),
-                ],
-              ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/logo_black.png",
+                  width: 200,
+                ),
+                Image.asset(
+                  "assets/images/logo_food_delivery.png",
+                  width: 110,
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -94,7 +92,31 @@ class MyApp extends StatelessWidget {
     return Expanded(
       flex: 5,
       child: Container(
-        color: Colors.orange,
+        child: Column(
+          children: [
+            _buildLoginButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _buildLoginButton() {
+    return Container(
+      width: 380,
+      height: 70,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        color: const Color.fromARGB(230, 255, 215, 0),
+      ),
+      child: const Center(
+        child: Text(
+          "Login",
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.black,
+          ),
+        ),
       ),
     );
   }
