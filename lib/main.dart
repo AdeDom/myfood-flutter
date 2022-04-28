4cc69ef01ec7933e7b588aeb3df3d60f7eb1ab29
pathiphon <pathiphon.ja@chococrm.com>
2022-04-28 22:13:20 +0700

    Build widget language en button.

--- lib/main.dart
+++ lib/main.dart
@@ -246,4 +246,79 @@ class MyApp extends StatelessWidget {
       ],
     );
   }
+
+  Widget _buildLanguageEnButton() {
+    return Stack(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  width: 80,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    border: Border.all(
+                      width: 1,
+                      color: const Color.fromARGB(230, 255, 215, 0),
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    const SizedBox(
+                      width: 40,
+                    ),
+                    Container(
+                      width: 40,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: const Color.fromARGB(230, 255, 215, 0),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: const [
+                SizedBox(height: 4,),
+                Text(
+                  "TH",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 14,
+            ),
+            Column(
+              children: const [
+                SizedBox(height: 4,),
+                Text(
+                  "EN",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
 }
