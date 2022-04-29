7418ff92e700cac334ec016104714c5c79cdd399
pathiphon <pathiphon.ja@chococrm.com>
2022-04-29 13:34:24 +0700

    Rename file name to snake case.

--- lib/presentation/welcome/welcome.dart
+++ lib/presentation/welcome/welcome.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:myfood/presentation/welcome/widget/BuildWelcomeBottomSection.dart';
-import 'package:myfood/presentation/welcome/widget/BuildWelcomeCenterSection.dart';
-import 'package:myfood/presentation/welcome/widget/BuildWelcomeTopSection.dart';
+import 'package:myfood/presentation/welcome/widget/build_welcome_bottom_section.dart';
+import 'package:myfood/presentation/welcome/widget/build_welcome_center_section.dart';
+import 'package:myfood/presentation/welcome/widget/build_welcome_top_section.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({Key? key}) : super(key: key);
--- lib/presentation/welcome/widget/build_welcome_bottom_section.dart
+++ lib/presentation/welcome/widget/build_welcome_bottom_section.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:myfood/presentation/welcome/widget/BuildLanguageButton.dart';
-import 'package:myfood/presentation/welcome/widget/BuildLoginButton.dart';
-import 'package:myfood/presentation/welcome/widget/BuildRegisterButton.dart';
-import 'package:myfood/presentation/welcome/widget/BuildSkipButton.dart';
+import 'package:myfood/presentation/welcome/widget/build_language_button.dart';
+import 'package:myfood/presentation/welcome/widget/build_login_button.dart';
+import 'package:myfood/presentation/welcome/widget/build_register_button.dart';
+import 'package:myfood/presentation/welcome/widget/build_skip_button.dart';
 
 class BuildWelcomeBottomSection extends StatelessWidget {
   const BuildWelcomeBottomSection({Key? key}) : super(key: key);
