eeb56a3525a7b1214251c48e90063c921485349f
pathiphon <pathiphon.ja@chococrm.com>
2022-04-29 14:03:53 +0700

    Build title and sub title.

--- lib/presentation/login/widget/build_login_top_section.dart
+++ lib/presentation/login/widget/build_login_top_section.dart
@@ -5,9 +5,37 @@ class BuildLoginTopSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-      height: 250,
+    return Column(
+      children: [
+        const SizedBox(
+          height: 64,
+        ),
+        _buildLoginTitle(),
+        const SizedBox(
+          height: 16,
+        ),
+        _buildLoginSubTitle(),
+      ],
+    );
+  }
+
+  Widget _buildLoginTitle() {
+    return const Text(
+      "Login",
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 32,
+      ),
+    );
+  }
+
+  Widget _buildLoginSubTitle() {
+    return const Text(
+      "Add your details to login",
+      style: TextStyle(
+        fontSize: 16,
+        color: Colors.grey,
+      ),
     );
   }
 }
