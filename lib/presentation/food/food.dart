5c55ecdde3249aac045dec16b52dbc89fb6f3ae3
pathiphon <pathiphon.ja@chococrm.com>
2022-05-02 02:24:09 +0700

    Create section on food page.

--- lib/presentation/food/food.dart
+++ lib/presentation/food/food.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:myfood/presentation/food_detail/food_detail.dart';
 
 class Food extends StatelessWidget {
   const Food({Key? key}) : super(key: key);
@@ -7,34 +6,46 @@ class Food extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const FoodDetail(foodId: 1),
-                  ),
-                );
-              },
-              child: const Text("Food detail 1"),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const FoodDetail(foodId: 2),
-                  ),
-                );
-              },
-              child: const Text("Food detail 2"),
-            ),
-          ],
-        ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              _buildTitleSection(),
+              _buildSearchSection(),
+              _buildCategoryListSection(),
+              _buildFoodListSection(),
+            ],
+          )
+        ],
       ),
+    );
+  }
+
+  Widget _buildTitleSection() {
+    return Container(
+      height: 300,
+      color: Colors.red,
+    );
+  }
+
+  Widget _buildSearchSection() {
+    return Container(
+      height: 300,
+      color: Colors.yellow,
+    );
+  }
+
+  Widget _buildCategoryListSection() {
+    return Container(
+      height: 300,
+      color: Colors.green,
+    );
+  }
+
+  Widget _buildFoodListSection() {
+    return Container(
+      height: 300,
+      color: Colors.orange,
     );
   }
 }
