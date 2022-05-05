b6f24ef76f2bb0c84bc315b5e25729577e776128
pathiphon <pathiphon.ja@chococrm.com>
2022-05-05 10:15:51 +0700

    Refactor base error.

--- lib/data/models/base/base_error.dart
+++ lib/data/models/base/base_error.dart
@@ -1,21 +1,21 @@
 class BaseError {
-  String? message;
   String? code;
+  String? message;
 
   BaseError({
-    this.message,
     this.code,
+    this.message,
   });
 
   BaseError.fromJson(Map<String, dynamic> json) {
-    message = json['message'];
     code = json['code'];
+    message = json['message'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
-    data['message'] = message;
     data['code'] = code;
+    data['message'] = message;
     return data;
   }
 }
