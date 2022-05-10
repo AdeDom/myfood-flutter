d7dbee7e07f324eb81c654de654f7040477806dc
pathiphon <pathiphon.ja@chococrm.com>
2022-05-10 19:25:00 +0700

    Migrate lib http to dio.

--- lib/data/providers/network/data_source_provider.dart
+++ lib/data/providers/network/data_source_provider.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:http/http.dart' as http;
-import 'package:http/http.dart';
+import 'package:dio/dio.dart';
 import 'package:myfood/data/models/base/base_error.dart';
 import 'package:myfood/data/providers/shared_preference/shared_preference.dart';
 
 class DataSourceProvider {
+  final _dio = Dio();
   static const String _baseUrl = "https://myfood-server.herokuapp.com/";
   static const String _statusKey = "status";
   static const String _successValue = "success";
@@ -20,9 +20,11 @@ class DataSourceProvider {
     String path,
     DataSourceType dataSourceType,
   ) async {
-    final response = http.get(
-      Uri.parse("$_baseUrl$path"),
-      headers: await _getHeaders(dataSourceType),
+    final response = await _dio.get(
+      "$_baseUrl$path",
+      options: Options(
+        headers: await _getHeaders(dataSourceType),
+      ),
     );
 
     return await _getResponse(response);
@@ -33,10 +35,12 @@ class DataSourceProvider {
     DataSourceType dataSourceType, {
     Object? body,
   }) async {
-    final response = http.post(
-      Uri.parse("$_baseUrl$path"),
-      headers: await _getHeaders(dataSourceType),
-      body: json.encode(body),
+    final response = await _dio.post(
+      "$_baseUrl$path",
+      options: Options(
+        headers: await _getHeaders(dataSourceType),
+      ),
+      data: body,
     );
 
     return await _getResponse(response);
@@ -58,10 +62,9 @@ class DataSourceProvider {
     return headers;
   }
 
-  Future<Map<String, dynamic>> _getResponse(Future<Response> response) async {
-    final _response = await response;
-    if (_response.statusCode == 200) {
-      final jsonResponse = json.decode(_response.body);
+  Future<Map<String, dynamic>> _getResponse(Response<dynamic> response) async {
+    if (response.statusCode == 200) {
+      final jsonResponse = response.data;
       if (jsonResponse[_statusKey] == _successValue) {
         return jsonResponse;
       } else {
@@ -71,7 +74,7 @@ class DataSourceProvider {
         throw ApiServiceManagerException(errorString);
       }
     } else {
-      final jsonResponse = json.decode(_response.body);
+      final jsonResponse = response.data;
       final error = jsonResponse[_errorValue];
       BaseError baseError = BaseError.fromJson(error);
       String errorString = json.encode(baseError);
