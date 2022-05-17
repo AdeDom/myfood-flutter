1c821d5e9f96c8e0f7f46e172fd95d9c94b0ceba
pathiphon <pathiphon.ja@chococrm.com>
2022-05-17 22:32:15 +0700

    Test case login success.

--- test/data/providers/network/auth/auth_remote_data_source.dart
+++ test/data/providers/network/auth/auth_remote_data_source.dart
@@ -1,19 +1,21 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:myfood/data/models/base/base_error.dart';
+import 'package:myfood/data/models/base/base_response.dart';
 import 'package:myfood/data/models/login/login_request.dart';
+import 'package:myfood/data/models/token/token.dart';
 import 'package:myfood/data/providers/network/api_service_manager.dart';
 import 'package:myfood/data/providers/network/auth/auth_remote_data_source.dart';
-import 'package:myfood/data/providers/store/data_store.dart';
 
-import '../../store/data_store.dart';
+class MockMyFoodDio extends Mock implements MyFoodDio {}
 
 void main() {
-  late DataStore dataStore;
   late MyFoodDio myFoodDio;
   late AuthRemoteDataSource dataSource;
 
   setUp(() {
-    dataStore = FakeDataStoreImpl();
-    myFoodDio = MyFoodDio(dataStore: dataStore);
+    myFoodDio = MockMyFoodDio();
     dataSource = AuthRemoteDataSourceImpl(myFoodDio: myFoodDio);
   });
 
@@ -28,13 +30,42 @@ void main() {
       email: email,
       password: password,
     );
+    String accessToken = "abc123";
+    String refreshToken = "xyz456";
+    Token token = Token(
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    );
+    String version = "1.0";
+    String status = "success";
+    BaseError? baseError;
+    final baseResponse = BaseResponse(
+      version: version,
+      status: status,
+      error: baseError,
+      result: token,
+    );
+    String path = "api/auth/login";
+    RequestOptions requestOptions = RequestOptions(path: path);
+    Response response = Response(
+      requestOptions: requestOptions,
+      data: baseResponse.toJson((value) => value.toJson()),
+    );
+    when(
+      () => myFoodDio.post(
+        path,
+        data: loginRequest,
+      ),
+    ).thenAnswer((_) => Future.value(response));
 
     final login = await dataSource.callLogin(loginRequest: loginRequest);
 
-    expect(login.version, "1.0");
-    expect(login.status, "success");
-    expect(login.error, null);
+    expect(login.version, version);
+    expect(login.status, status);
+    expect(login.error, baseError);
     expect(login.result, isNotNull);
+    expect(login.result?.accessToken, accessToken);
+    expect(login.result?.refreshToken, refreshToken);
   });
 
   test("callLogin_returnError", () async {
