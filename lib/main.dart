f0f2cdd356bb01bb5542662abd993866272a33c1
pathiphon <pathiphon.ja@chococrm.com>
2022-06-07 16:35:38 +0700

    Setup get x.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:myfood/app/config/database_constant.dart';
+import 'package:myfood/app/routes/app_pages.dart';
 import 'package:myfood/data/models/user/user_entity.dart';
 import 'package:myfood/data/providers/store/data_store.dart';
 import 'package:myfood/presentation/food/food.dart';
@@ -29,8 +31,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       routes: _route,
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
       title: 'My Food',
       theme: ThemeData(
         primarySwatch: Colors.yellow,
