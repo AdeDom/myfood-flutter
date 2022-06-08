7374b1f7dcfaeffa20377b1a9599f27bc29b0ffa
pathiphon <pathiphon.ja@chococrm.com>
2022-06-08 14:26:01 +0700

    Initial feature register.

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -7,6 +7,7 @@ abstract class Routes {
   static const SPLASH_SCREEN = _Paths.SPLASH_SCREEN;
   static const WELCOME = _Paths.WELCOME;
   static const LOGIN = _Paths.LOGIN;
+  static const REGISTER = _Paths.REGISTER;
 }
 
 abstract class _Paths {
@@ -16,4 +17,5 @@ abstract class _Paths {
   static const SPLASH_SCREEN = '/splash-screen';
   static const WELCOME = '/welcome';
   static const LOGIN = '/login';
+  static const REGISTER = '/register';
 }
