2a1e76a11a15b392e6a85517f7a34cc00c0e43cd
pathiphon <pathiphon.ja@chococrm.com>
2022-06-10 16:26:11 +0700

    Create widget category list.

--- lib/app/modules/home/views/home_page.dart
+++ lib/app/modules/home/views/home_page.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:get/get.dart';
 import 'package:myfood/app/config/app_constant.dart';
+import 'package:myfood/app/data/models/category/category.dart';
 import 'package:myfood/app/modules/home/controllers/home_controller.dart';
 import 'package:myfood/app/routes/app_pages.dart';
 
 class HomePage extends GetView<HomeController> {
-  const HomePage({Key? key}) : super(key: key);
+  final List<Category>? categoryList;
+
+  const HomePage({
+    Key? key,
+    this.categoryList,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -77,11 +83,50 @@ class HomePage extends GetView<HomeController> {
   }
 
   Widget _buildCategoryListSection() {
-    return Container(
-      width: double.infinity,
-      height: 100,
-      color: Colors.deepOrangeAccent,
-    );
+    if (categoryList == null) {
+      return Container();
+    } else {
+      return Padding(
+        padding: const EdgeInsets.only(left: 8),
+        child: SizedBox(
+          height: 150,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: categoryList?.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: ClipRRect(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(12),
+                        ),
+                        child: Image.network(
+                          categoryList![index].image ?? "",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      categoryList![index].categoryName ?? "",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      );
+    }
   }
 
   Widget _buildFoodListSection() {
