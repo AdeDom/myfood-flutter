52cef2d28847427e17f252bb9e4c367d84433c8f
pathiphon <pathiphon.ja@chococrm.com>
2022-06-14 10:17:20 +0700

    Call service by async data.

--- lib/app/data/repositories/food/food_repository.dart
+++ lib/app/data/repositories/food/food_repository.dart
@@ -1,3 +1,5 @@
+import 'package:myfood/app/data/models/base/base_response.dart';
+import 'package:myfood/app/data/models/food/food.dart';
 import 'package:myfood/app/data/models/food/food_entity.dart';
 import 'package:myfood/app/data/providers/database/category/category_local_data_source.dart';
 import 'package:myfood/app/data/providers/database/food/food_local_data_source.dart';
@@ -17,35 +19,41 @@ class FoodRepositoryImpl with FoodRepository {
 
   @override
   Future<void> callFoodListByCategoryId() async {
-    await foodLocalDataSource.deleteFoodAll();
     final categoryAll = categoryLocalDataSource.getCategoryAll();
+    List<Future<BaseResponse<List<Food>>>> futureList = [];
     categoryAll?.forEach((element) async {
       int? categoryId = element.categoryId;
       if (categoryId != null) {
-        final foodResponse = await foodRemoteDataSource.callFoodListByCategoryId(
+        final foodResponse = foodRemoteDataSource.callFoodListByCategoryId(
           categoryId: categoryId,
         );
-        final foodList = foodResponse.result;
-        if (foodList != null) {
-          List<FoodEntity> foodEntity = foodList.map((food) {
-            return FoodEntity(
-              foodId: food.foodId,
-              foodName: food.foodName,
-              alias: food.alias,
-              image: food.image,
-              price: food.price,
-              description: food.description,
-              favorite: food.favorite,
-              ratingScore: food.ratingScore,
-              ratingScoreCount: food.ratingScoreCount,
-              categoryId: food.categoryId,
-              status: food.status,
-              created: food.created,
-              updated: food.updated,
-            );
-          }).toList();
-          await foodLocalDataSource.saveFoodList(foodEntity);
-        }
+        futureList.add(foodResponse);
+      }
+    });
+
+    final foodListAdd = await Future.wait<BaseResponse<List<Food>>>(futureList);
+    await foodLocalDataSource.deleteFoodAll();
+    foodListAdd.forEach((element) async {
+      final foodList = element.result;
+      if (foodList != null) {
+        List<FoodEntity> foodEntity = foodList.map((food) {
+          return FoodEntity(
+            foodId: food.foodId,
+            foodName: food.foodName,
+            alias: food.alias,
+            image: food.image,
+            price: food.price,
+            description: food.description,
+            favorite: food.favorite,
+            ratingScore: food.ratingScore,
+            ratingScoreCount: food.ratingScoreCount,
+            categoryId: food.categoryId,
+            status: food.status,
+            created: food.created,
+            updated: food.updated,
+          );
+        }).toList();
+        await foodLocalDataSource.saveFoodList(foodEntity);
       }
     });
   }
