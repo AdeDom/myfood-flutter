b51abd6b593c652876b8a1e6548252c646ba7724
pathiphon <pathiphon.ja@chococrm.com>
2022-06-14 15:58:00 +0700

    Create food detail page.

--- lib/app/modules/food_detail/views/food_detail_view.dart
+++ lib/app/modules/food_detail/views/food_detail_view.dart
@@ -9,7 +9,165 @@ class FoodDetailView extends GetView<FoodDetailController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Food detail (foodId = ${Get.arguments})"),
+      body: _buildFoodDetailWidget(),
+    );
+  }
+
+  Widget _buildFoodDetailWidget() {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          expandedHeight: Get.height / 2,
+          flexibleSpace: FlexibleSpaceBar(
+            background: _buildTopSection(),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              Container(
+                height: Get.height,
+                color: Colors.white,
+                child: _buildBottomSection(),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTopSection() {
+    return Stack(
+      children: [
+        SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: Image.network(
+            "https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg",
+            fit: BoxFit.cover,
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              width: double.infinity,
+              height: 32,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(64),
+                  topRight: Radius.circular(64),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade500,
+                    spreadRadius: 5,
+                    blurRadius: 10,
+                  )
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(64),
+                  topRight: Radius.circular(64),
+                ),
+                child: Container(
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 16),
+                child: Image.asset(
+                  "assets/images/favorite_active.png",
+                  width: 80,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBottomSection() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 32,
+        right: 32,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Tandoori Chicken Pizza",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Row(
+            children: [
+              const Icon(
+                Icons.star,
+                size: 18,
+                color: Colors.amber,
+              ),
+              const SizedBox(width: 4),
+              const Text(
+                "4.9",
+                style: TextStyle(
+                  color: Colors.amber,
+                  fontSize: 14,
+                ),
+              ),
+              Expanded(
+                child: Container(),
+              ),
+              const Text(
+                "750",
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(width: 8),
+              const Text(
+                "Bath",
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 32),
+          const Text(
+            "Description",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(height: 8),
+          const Text(
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
