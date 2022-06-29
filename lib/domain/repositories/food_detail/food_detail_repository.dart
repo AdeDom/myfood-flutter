b8fcc2fcafe6fa3ecd9940b487c5fea51df26a96
pathiphon <pathiphon.ja@chococrm.com>
2022-06-29 18:20:41 +0700

    Refactor handle logic from food detail repository to domain layer.

--- lib/domain/repositories/food_detail/food_detail_repository.dart
+++ lib/domain/repositories/food_detail/food_detail_repository.dart
@@ -1,8 +1,7 @@
 import 'package:myfood/app/data/models/food/food.dart';
-import 'package:myfood/app/data/repositories/result.dart';
 
 mixin FoodDetailRepository {
-  Future<Result<Food>> callFoodDetail({
+  Future<Food?> callFoodDetail({
     required int foodId,
   });
 }
--- lib/domain/usecases/food_detail/get_food_detail_use_case.dart
+++ lib/domain/usecases/food_detail/get_food_detail_use_case.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:myfood/app/data/models/base/base_error.dart';
 import 'package:myfood/app/data/models/food/food.dart';
+import 'package:myfood/app/data/providers/network/api_service_manager.dart';
 import 'package:myfood/app/data/repositories/result.dart';
 import 'package:myfood/domain/repositories/food_detail/food_detail_repository.dart';
 
@@ -10,9 +13,20 @@ class GetFoodDetailUseCase {
     required this.foodDetailRepository,
   });
 
-  Future<Result<Food>> call({int? foodId}) {
+  Future<Result<Food>> call({int? foodId}) async {
     if (foodId != null) {
-      return foodDetailRepository.callFoodDetail(foodId: foodId);
+      try {
+        final food = await foodDetailRepository.callFoodDetail(foodId: foodId);
+        return Result.success(data: food);
+      } on ApiServiceManagerException catch (error) {
+        Map<String, dynamic> jsonError = json.decode(error.message);
+        BaseError baseError = BaseError.fromJson(jsonError);
+        return Result.error(baseError);
+      } catch (error) {
+        Map<String, dynamic> jsonError = json.decode(error.toString());
+        BaseError baseError = BaseError.fromJson(jsonError);
+        return Result.error(baseError);
+      }
     } else {
       String message = "Food id is null.";
       BaseError error = BaseError(message: message);
